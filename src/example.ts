050efb711b6a4fc4e5c611758feb4fe0ccbe7441
takashi <tks.srtr89@gmail.com>
2024-03-31 19:45:55 +0900

    add tsconfig , tsc script

---
+++ src/example.ts
@@ -0,0 +1,14 @@
+type Todo = {
+  title: string
+  priority: "high"| "middle"| "low";
+}
+
+const todo1: Todo = {
+  title: 'title1',
+  priority: 'high',
+}
+
+const todo2: Todo = {
+  title: 'foo',
+  priority: 'low',
+}
