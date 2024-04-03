3b6cfbd68fce01a8c99781fffbce8680995ac9c9
takashi <tks.srtr89@gmail.com>
2024-04-03 21:39:45 +0900

    fix: dir

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@
 
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import { plugin as eslintPluginExample } from './dist-eslint-rules/index.js'
+import { plugin as eslintPluginExample } from './eslint-rules/out/index.js'
 
 export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
   files: ['src/**/*.ts'],
